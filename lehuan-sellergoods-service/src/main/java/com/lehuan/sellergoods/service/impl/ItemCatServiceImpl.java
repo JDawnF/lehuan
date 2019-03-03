bbd7f09950ddf4ba00bf73b60a64bb7a062f505d
JDawnF <335825732@qq.com>
2019-03-03 16:47:33 +0800

    商品分类service和controller

---
+++ lehuan-sellergoods-service/src/main/java/com/lehuan/sellergoods/service/impl/ItemCatServiceImpl.java
@@ -0,0 +1,134 @@
+package com.lehuan.sellergoods.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.redis.core.RedisTemplate;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.lehuan.mapper.TbItemCatMapper;
+import com.lehuan.pojo.TbItemCat;
+import com.lehuan.pojo.TbItemCatExample;
+import com.lehuan.pojo.TbItemCatExample.Criteria;
+import com.lehuan.sellergoods.service.ItemCatService;
+
+import entity.PageResult;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 商品分类服务实现层
+ *
+ * @author Administrator
+ */
+@Service
+@Transactional
+public class ItemCatServiceImpl implements ItemCatService {
+
+    @Autowired
+    private TbItemCatMapper itemCatMapper;
+//    @Autowired
+//    private RedisTemplate redisTemplate;
+
+    /**
+     * 查询全部
+     */
+    @Override
+    public List<TbItemCat> findAll() {
+        return itemCatMapper.selectByExample(null);
+    }
+
+    /**
+     * 按分页查询
+     */
+    @Override
+    public PageResult findPage(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        Page<TbItemCat> page = (Page<TbItemCat>) itemCatMapper.selectByExample(null);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
+    /**
+     * 增加
+     */
+    @Override
+    public void add(TbItemCat itemCat) {
+        itemCatMapper.insert(itemCat);
+    }
+
+
+    /**
+     * 修改
+     */
+    @Override
+    public void update(TbItemCat itemCat) {
+        itemCatMapper.updateByPrimaryKey(itemCat);
+    }
+
+    /**
+     * 根据ID获取实体
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public TbItemCat findOne(Long id) {
+        return itemCatMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * 批量删除
+     */
+    @Override
+    public void delete(Long[] ids) {
+        for (Long id : ids) {
+            itemCatMapper.deleteByPrimaryKey(id);
+        }
+    }
+
+
+    @Override
+    public PageResult findPage(TbItemCat itemCat, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+
+        TbItemCatExample example = new TbItemCatExample();
+        Criteria criteria = example.createCriteria();
+
+        if (itemCat != null) {
+            if (itemCat.getName() != null && itemCat.getName().length() > 0) {
+                criteria.andNameLike("%" + itemCat.getName() + "%");
+            }
+
+        }
+
+        Page<TbItemCat> page = (Page<TbItemCat>) itemCatMapper.selectByExample(example);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
+
+    //    @Override
+//    public List<TbItemCat> findByParentId(Long parentId) {
+//        TbItemCatExample example = new TbItemCatExample();
+//        Criteria criteria = example.createCriteria();
+//        // 设置条件，等于parentId即可
+//        criteria.andParentIdEqualTo(parentId);
+//        // 条件查询
+//		//将模板ID放入缓存（以商品分类名称作为key）
+////     每次执行查询的时候，一次性读取缓存进行存储 (因为每次增删改都要执行此方法)
+//        List<TbItemCat> itemCatList = findAll();  //查询所有分类
+//        for (TbItemCat itemCat : itemCatList) {
+//            redisTemplate.boundHashOps("itemCat").put(itemCat.getName(), itemCat.getTypeId());
+//        }
+//        //System.out.println("将模板ID放入缓存");
+//        return itemCatMapper.selectByExample(example);
+//    }
+    @Override
+    public List<TbItemCat> findByParentId(Long parentId) {
+        TbItemCatExample example = new TbItemCatExample();
+        Criteria criteria = example.createCriteria();
+        criteria.andParentIdEqualTo(parentId);
+        return itemCatMapper.selectByExample(example);
+    }
+}
